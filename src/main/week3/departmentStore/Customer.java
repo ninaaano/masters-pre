bb6d6b7abe075c06356a0aba13ff96bc23c99f1d
Life is a ninanino <tvxqinak@naver.com>
2022-11-18 00:05:44 +0900

    1번미션 백화점 enum활용

---
+++ src/main/week3/departmentStore/Customer.java
@@ -0,0 +1,4 @@
+package main.week3.departmentStore;// @ author ninaaano
+
+public class Customer {
+}
---
+++ src/main/week3/departmentStore/CustomerRating.java
@@ -0,0 +1,3 @@
+package main.week3.departmentStore;// @ author ninaaano
+public enum CustomerRating {
+}
