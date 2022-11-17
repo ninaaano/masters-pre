6c4b4e478548fe2bf5df37d2c88fb2c17963b79e
Life is a ninanino <tvxqinak@naver.com>
2022-11-18 03:29:02 +0900

    왜 날아갔지???

---
+++ src/main/week3/CoffeeShop/BrazilAmericano.java
@@ -0,0 +1,12 @@
+package main.week3.CoffeeShop;// @ author ninaaano
+
+public class BrazilAmericano implements Coffee {
+
+    public BrazilAmericano() {
+    }
+
+    @Override
+    public void brewing() {
+        System.out.print("BrazilAmericano");
+    }
+}
