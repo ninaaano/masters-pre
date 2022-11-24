13f6181cc668d56fed5c6a777ae1b1754b805ed5
Life is a ninanino <tvxqinak@naver.com>
2022-11-24 17:39:00 +0900

    refactor : 변경 내용 커밋

--- src/main/week4/MemberShip/HashMap/MemberShipMangementMap.java
+++ src/main/week4/MemberShip/HashMap/MemberShipMangementMap.java
@@ -8,20 +8,33 @@ import java.util.Map;
 
 public class MemberShipMangementMap {
 
+    private HashMap<Integer, Member> memberMap;
+
     public MemberShipMangementMap() {
         this.memberMap = new HashMap<>();
     }
 
-     Map<Member> memberMap;
 
-    public void addMemberShip(int i, String kim, GRADE platinum) {
+    public void addMemberShip(int id, String name, GRADE grade) {
+        if(memberMap.containsKey(id)){
+            System.out.println("이미 있는 아이디"+id+"의 값이 변경됩니다.");
+        }
+        memberMap.put(id,new Member(id,name,grade));
     }
 
-    public boolean removeMember(int i) {
+    public boolean removeMember(int id) {
+            if (memberMap.containsKey(id)) {
+                memberMap.remove(id);
+                return true;
+            }
+        System.out.println(id + "에 해당하는 멤버가 없습니다.");
         return false;
     }
 
     public void showAllMember() {
+        System.out.println("=========================");
+        memberMap.forEach((id , member) -> System.out.println(member.viewInfo()));
+        System.out.println("=========================");
     }
 
 
