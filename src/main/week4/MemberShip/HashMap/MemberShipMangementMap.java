68114e33b621b58bde80a33441ffeb09555b6b27
Life is a ninanino <tvxqinak@naver.com>
2022-11-24 17:38:13 +0900

    221124 17:37 : week4_memberShip_HashMap 커밋

---
+++ src/main/week4/MemberShip/HashMap/MemberShipMangementMap.java
@@ -0,0 +1,28 @@
+package main.week4.MemberShip.HashMap;// @ author ninaaano
+
+import main.week4.MemberShip.membership.GRADE;
+import main.week4.MemberShip.membership.Member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemberShipMangementMap {
+
+    public MemberShipMangementMap() {
+        this.memberMap = new HashMap<>();
+    }
+
+     Map<Member> memberMap;
+
+    public void addMemberShip(int i, String kim, GRADE platinum) {
+    }
+
+    public boolean removeMember(int i) {
+        return false;
+    }
+
+    public void showAllMember() {
+    }
+
+
+}
