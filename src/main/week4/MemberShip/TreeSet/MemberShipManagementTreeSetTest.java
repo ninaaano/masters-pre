c0d61c4b0225d4cf1279abecc2457f2544802cbf
Life is a ninanino <tvxqinak@naver.com>
2022-11-25 22:21:22 +0900

    221025 22:21 : first commit

--- src/main/week4/MemberShip/TreeSet/MemberShipManagementTreeSetTest.java
+++ src/main/week4/MemberShip/TreeSet/MemberShipManagementTreeSetTest.java
@@ -1,3 +1,36 @@
 package main.week4.MemberShip.TreeSet;// @ author ninaaano
+// #Binary Search Tree #key 값을 비교하는 방법 #CallBack함수의 의미
+
+import main.week4.MemberShip.membership.GRADE;
+
 public class MemberShipManagementTreeSetTest {
+    public static void main(String[] args) {
+
+
+        MemberShipMangementTreeSet management = new MemberShipMangementTreeSet();
+        management.addMemberShip(1003, "Hong", GRADE.SILVER);
+        management.addMemberShip(1001, "Park", GRADE.VIP);
+        management.addMemberShip(1002, "Kim", GRADE.PLATINUM);
+
+        System.out.println("모든 멤버 보여주기: ");
+        management.showAllMember();
+
+        System.out.println("존재 하는 아이디를 지웠을때 :");
+        if(management.removeMember(1001)) {
+            management.showAllMember();
+        }
+
+        System.out.println("존재 하지 않는 아이디를 지우려 할때:");
+        if(management.removeMember(1005)) {
+            management.showAllMember();
+        }
+
+
+        System.out.println("이미 존재 하는 아이디를 추가하려 할때:");
+        management.addMemberShip(1002, "Lee", GRADE.VIP);
+        // 이름으로 비교하는 경우
+        // System.out.println("이미 존재 하는 이름을 추가하려 할때:");
+        // management.addMemberShip(1002, "Kim", GRADE.VIP);
+        management.showAllMember();
+    }
 }
--- src/main/week4/MemberShip/TreeSet/MemberShipMangementTreeSet.java
+++ src/main/week4/MemberShip/TreeSet/MemberShipMangementTreeSet.java
@@ -1,3 +1,15 @@
 package main.week4.MemberShip.TreeSet;// @ author ninaaano
+
+import main.week4.MemberShip.membership.GRADE;
+
 public class MemberShipMangementTreeSet {
+    public void addMemberShip(int i, String hong, GRADE silver) {
+    }
+
+    public boolean removeMember(int i) {
+        return false;
+    }
+
+    public void showAllMember() {
+    }
 }
